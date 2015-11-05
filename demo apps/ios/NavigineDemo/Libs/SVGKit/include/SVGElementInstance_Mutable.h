e3173fcadde3dcca1548d848802137aad97c572d
Alex <aleksei.smirnov@navigine.com>
2015-11-06 01:06:17 +0300

    Demo apps folder was deleted as it was moved to another repo

--- demo apps/ios/NavigineDemo/Libs/SVGKit/include/SVGElementInstance_Mutable.h
+++
@@ -1,8 +0,0 @@
-#import "SVGElementInstance.h"
-
-@interface SVGElementInstance ()
-@property(nonatomic,retain, readwrite) SVGElement* correspondingElement;
-@property(nonatomic,retain, readwrite) SVGUseElement* correspondingUseElement;
-@property(nonatomic,retain, readwrite) SVGElementInstance* parentNode;
-@property(nonatomic,retain, readwrite) SVGElementInstanceList* childNodes;
-@end
